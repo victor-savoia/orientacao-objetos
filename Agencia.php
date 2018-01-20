4881252f68a861df9efb5d9327ba96f1a45cfe96
php 19 <php.19@penadinho.cec.ime.usp.br>
2018-01-19 22:32:36 -0200

    adicao da classe agencia

---
+++ Agencia.php
@@ -0,0 +1,21 @@
+<?php
+
+  class Agencia{
+    public $numero;
+  }
+
+  $c1 = new Agencia();
+
+  $c1->numero = 1234;
+
+  echo $c1->numero . "\n";
+
+  echo "\n";
+
+  $c2 = new Agencia();
+
+  $c2->numero = 5678;
+
+  echo $c2->numero . "\n";
+
+ ?>
